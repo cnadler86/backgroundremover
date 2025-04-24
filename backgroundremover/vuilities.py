10ab9896beff6de974fca627e56cedbb3969a2e3
Christopher Nadler <christopher.nadler@sick.de>
2025-04-24 15:29:21 +0000

    minor improvements

--- backgroundremover/vuilities.py
+++ backgroundremover/vuilities.py
@@ -4,9 +4,11 @@ import ffmpeg
 from pydantic import FilePath
 
 def transparentvideo_bluescreen_compressed(file_path, matte_key_file, output=None,*, mode="slow", bg_color="blue"):
-    # Get video information
+    # Inputs
     file_path= os.path.abspath(file_path)
     matte_key_file= os.path.abspath(matte_key_file)
+    preset = "slow" if mode == "slow" else "fast"
+
     probe = ffmpeg.probe(file_path)
     video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
     width = int(video_info['width'])
@@ -18,20 +20,9 @@ def transparentvideo_bluescreen_compressed(file_path, matte_key_file, output=Non
         output = base + "_out" + ".mp4"
 
     # Adjust preset based on mode
-    preset = "slow" if mode == "slow" else "fast"
+
 
     # Using the matte as a mask to blend between original video and solid color
-    # cmd = [
-    #     'ffmpeg', '-y',
-    #     '-i', file_path,      # Input 0: Original video
-    #     '-i', matte_key_file,      # Input 1: Matte video
-    #     '-f', 'lavfi',
-    #     '-i', f'color=c={bg_color}:s={width}x{height}:r=25',  # Input 2: Solid color with matching dimensions
-    #     '-filter_complex',
-    #     '[0:v][1:v]scale2ref[main][mask];[2:v][main]scale2ref=flags=neighbor[bg][main];[main][mask][bg]maskedmerge',
-    #     '-c:v', 'libx264', '-crf', '0', '-preset', preset, '-pix_fmt', 'yuv420p', '-shortest', output
-    # ]
-
     cmd = [
         'ffmpeg', '-y',
         '-i', file_path,
